8b1f0b11fc7cffc2ec5f272b671233514e5e8708
ilton barbosa <jiltonbarbosa@gmail.com>
2025-08-05 11:53:09 -0300

    chore: add better-auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config';
 
 import { drizzle } from 'drizzle-orm/node-postgres';
-export const db = drizzle(process.env.DATABASE_URL!);
+
+import * as schema from './schema'; //para o db saber quais schema está sendo utilizado
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema });
