a38cea26ec330981d3d49ada86fd0212361f10ca
m264755 <ilton.barbosa@embrapa.br>
2025-08-06 15:22:31 -0300

    feat: iniciando o catálogo de produtos

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
 
  const match = firstVariant.imageUrl.match(/https?:\/\/[^"]+/);
@@ -21,11 +23,12 @@ const ProductItem = ({ product }: ProductItemProps) => {
       <Image
         src={imageUrl}
         alt={firstVariant.name}
-        width={200}
-        height={200}
+        sizes="100vw"
+        width={0}
+        height={0}
         className="rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div className={cn("flex max-w-[200px] flex-col gap-1", textContainerClassName)}>
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
